e9f3093ffce665a99af07a3da5e5da6ae0eceed2
Aditi <135489805+aditibadkur@users.noreply.github.com>
2024-05-12 10:20:34 +0530

    corrected v

--- OS/disk-scheduling/FCFS.c
+++ OS/disk-scheduling/FCFS.c
@@ -11,19 +11,6 @@ void print(int arr[]){
     printf("\n");
 }
 
-void sort(int arr[]){
-    for(i=0;i<n;i++){
-        for(j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                int temp = arr[j+1];
-                arr[j+1] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
-    print(tracks);
-}
-
 void fcfs(){
     print(tracks);
     for(i=0;i<n;i++){ // for finding index
