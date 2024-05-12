a3eac584fa4bfcc5ce2a23fb0df4c2ceedb2f097
Aditi <135489805+aditibadkur@users.noreply.github.com>
2024-05-12 11:31:30 +0530

    updated prod-con

--- OS/producer-consumer/producer-consumer.c
+++ OS/producer-consumer/producer-consumer.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define SIZE 3
+#define SIZE 10
 int count=0, mutex=1, full=0, empty=SIZE;
 // mutex = 1 available
 
@@ -32,31 +32,61 @@ void consumer(){
     mutex = signal(mutex);
 }
 
+#include<stdio.h>
+#include<stdlib.h>
+int c, mutex=1, full=0, empty=10, count=0;
+
+int wait(int s){
+    return --s; // blocks semaphore
+}
+
+int signal(int s){
+    return ++s; // releases semaphore
+}
+
+void producer(){
+    mutex = wait(mutex); // block resource
+    full = signal(full);
+    empty = wait(empty);
+    count++;
+    printf("\nProducer produces item %d", count);
+    mutex = signal(mutex);
+}
+
+void consumer(){
+    mutex = wait(mutex); // block resource
+    full = wait(full);
+    empty = signal(empty);
+    printf("\nConsumer consumes item %d", count);
+    count--;
+    mutex = signal(mutex);
+}
+
 void main(){
-    int choice;
+    int arr[20] = {1, 2};
     while(1){
-        printf("\n1.Producer\n2.Consumer\n3.Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-        switch (choice)
-        {
-        case 1: 
-        if((mutex == 1) && (empty != 0)) 
-            producer();
-        else 
-            printf("\nBuffer is full!\n");
-        break;
-
-        case 2: 
-        if((mutex == 1) && (full != 0)) 
-            consumer();
-        else 
-            printf("\nBuffer is empty!\n");
-        break;
-
-        case 3: exit(0);
-        
-        default: printf("\nEnter valid choice");
+        int c = arr[rand() % 2];
+        switch(c){
+            case 1: 
+            if(mutex == 1 && empty!=0){
+                producer();
+            }
+            else{
+                printf("\nBuffer is full!");
+            }
+            break;
+            
+            case 2:
+            if(mutex == 1 && full!=0){
+                consumer();
+            }
+            else{
+                printf("\nBuffer is empty!");
+            }
+            break;
+            
+            case 3: exit(0);
         }
+        sleep(2);
     }
 }
